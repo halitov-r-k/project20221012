cf828cda866578b2b4d542eb7f84dcaac2a31c29
ee-r <halitov.r.k@gmail.com>
2022-10-13 12:06:39 +0500

    2022-10-13T12:05

---
+++ src/main/java/ru/halitov/TestSpring.java
@@ -0,0 +1,15 @@
+package ru.halitov;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        TestBean testBean = context.getBean("testBeanId", TestBean.class);
+        System.out.println(testBean.getName());
+
+    }
+}
