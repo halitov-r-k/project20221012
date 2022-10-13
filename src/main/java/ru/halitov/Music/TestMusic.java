2fd9f6cfdd8019457f2a67e5e6a971a5028a0725
halitov-r-k <halitov.r.k@gmail.com>
2022-10-13 23:14:25 +0500

    2022-10-13T23:13

--- src/main/java/ru/halitov/Music/TestMusic.java
+++ src/main/java/ru/halitov/Music/TestMusic.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestMusic {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBeanId", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+      //  Music music = context.getBean("musicBeanId", Music.class);
+      //  MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayerBeanId", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
