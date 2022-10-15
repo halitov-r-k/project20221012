6792afec141e7ef4aa5c4005277be345b6bc4980
halitov-r-k <halitov.r.k@gmail.com>
2022-10-15 13:10:35 +0500

    2022-10-15T13:10

--- src/main/java/ru/halitov/Music/TestMusic.java
+++ src/main/java/ru/halitov/Music/TestMusic.java
@@ -7,10 +7,20 @@ public class TestMusic {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
       //  Music music = context.getBean("musicBeanId", Music.class);
       //  MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayerBeanId", MusicPlayer.class);
+        /*
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayerBeanId", MusicPlayer.class);
         musicPlayer.playMusic();
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+        */
+
+
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayerBeanId", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayerBeanId", MusicPlayer.class);
+        musicPlayer1.setVolume(51);
+        musicPlayer2.setVolume(52);
+        System.out.println((musicPlayer1==musicPlayer2 ) +" "+ musicPlayer1.toString() +" "+ musicPlayer2.toString());
+        System.out.println("musicPlayer1.volume: " + musicPlayer1.getVolume() +" " + "musicPlayer2.volume: " + musicPlayer2.getVolume() );
         context.close();
     }
 }
