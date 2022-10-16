d16d3b4b04cfcfab93da4c38c4dae52f985f4fcc
halitov-r-k <halitov.r.k@gmail.com>
2022-10-16 09:37:41 +0500

    2022-10-16T09:40

---
+++ src/main/java/ru/halitov/web/controllers/helloController.java
@@ -0,0 +1,10 @@
+package ru.halitov.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class helloController {
+    @GetMapping("/hello")
+    public String sayHello() {return "hello";}
+}
