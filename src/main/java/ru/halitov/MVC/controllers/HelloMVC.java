d3fb2c2cf109b5503963f11eb2c4fb82b6de917f
halitov-r-k <halitov.r.k@gmail.com>
2022-10-27 07:03:22 +0500

    2022-10-27T07:00

--- src/main/java/ru/halitov/MVC/controllers/HelloMVC.java
+++ src/main/java/ru/halitov/MVC/controllers/HelloMVC.java
@@ -1,10 +1,17 @@
 package ru.halitov.MVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloMVC {
 @GetMapping("/")
-    public String sayHelloMVC() {return "helloMVC";}
+    public String sayHelloMVC(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "volume", required = false) Integer volume,
+                              Model model) {
+   // System.out.println("name: " + name + " volume: " + volume);
+    model.addAttribute("message", "name: " + name + " volume: " + volume);
+    return "helloMVC";}
 }
